8d5b071c7e2511f4eba71db70126ec21a85f9aac
greyhexy <whatoget@gmail.com>
2021-07-25 23:24:52 +0800

    add drawing lines between social figure

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 import requests as req
 
+from pprint import pprint
 from tqdm import tqdm
 from config import headers, cookies, info_url, follower_url
 
@@ -34,6 +35,10 @@ class RelationShip:
             return False
         return self.subject == other.subject and self.follower == other.follower
 
+    def __lt__(self, other):
+        return self.subject < other.subject \
+               or (self.subject == other.subject and self.follower < other.follower)
+
 
 class User:
     def __init__(self, uid, name, **kargs):
@@ -44,6 +49,12 @@ class User:
 
     def get_posts(self):
         pass
+
+    @staticmethod
+    def construct_user(uid: int):
+        resp = get_user_info(uid)
+        name = resp.pop('screen_name')
+        return User(uid, name, **resp)
 
     def __hash__(self):
         return hash(self.uid)
@@ -69,18 +80,40 @@ class User:
 # 只关注与根用户互相关注的好友圈
 def accept(people: User, depth):
     return depth or people.follow_me
+    # return True
 
 
 class Network:
     logger = None
 
     def __init__(self, relations: list[RelationShip], users: list[User], name=None):
-        self.relations = relations
-        self.users = users
+        self.relations = sorted(relations)
+        self.users = {user.uid: user for user in users}
         self.name = name
+        # 保存关系列表中相同subject的关系在列表中的最小索引
+        self.head = dict()
+        # 保存关系列表中相同subject的关系数目
+        self.length = dict()
+        self.construct_lookup()
+
+    def construct_lookup(self):
+        """根据关系列表构造head与length字典
+        """
+        for idx, r in enumerate(self.relations):
+            _from = r.subject
+            _to = r.follower
+            item = self.head.setdefault(_from, idx)
+            if item != idx:
+                self.head[_from] = min(item, idx)
+            item = self.length.setdefault(_from, 1)
+            if item != 1:
+                self.length[_from] += 1
+
+    def get_user(self, uid):
+        return self.users.get(uid)
 
     def add_user(self, user: User):
-        self.users.append(user)
+        self.users[user.uid] = user
 
     def remove_user(self, uid):
         self.remove_relation(uid)
@@ -105,12 +138,13 @@ class Network:
         merge a subnet to form a bigger network
         :param net: subnet
         """
+        self.users.update(net.users)
         self.relations.extend(net.relations)
-        self.users.extend(net.users)
-        self.users = list(set(self.users))
+        self.relations.sort()
 
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, indent=4, ensure_ascii=False)
+        return json.dumps(self,
+                          default=lambda o: o.__dict__, indent=4, ensure_ascii=False)
 
     def from_json(self, fp):
         return json.load(self, fp)
@@ -123,16 +157,21 @@ class Network:
     def construct(uid: int, max_depth=1, depth=0, callback=None):
         if depth >= max_depth:
             return
+        # 获取当前uid所关注的用户列表
         users = list(filter(lambda x: accept(x, depth), get_followers(uid)))
         relations = [RelationShip(uid, user.uid) for user in users]
         net = Network(relations, users)
-        container = net.users[:] if depth else tqdm(net.users[:])
-        for user in container:
-            out_net = Network.construct(user.uid, max_depth, depth + 1)
+        container = net.users.keys() if depth else tqdm(net.users.keys())
+        # 递归获取关注用户所关注的用户
+        for id in container:
+            out_net = Network.construct(id, max_depth, depth + 1)
             if out_net:
                 net.merge(out_net)
             if callback:
                 callback(net)
+        # 将当前uid对应的用户加入社交网络
+        current = User.construct_user(uid)
+        net.add_user(current)
         return net
 
     @property
@@ -161,7 +200,7 @@ class Spider:
         pass
 
 
-def get_user_info(uid):
+def get_user_info(uid) -> dict:
     url = info_url.replace('{id}', str(uid))
     resp = req.get(url, headers=headers, cookies=cookies).json()
     assert resp.get('data') and resp['data'].get('user'), f'response of get {url}: {resp}'
@@ -212,6 +251,7 @@ def get_logger():
 if __name__ == "__main__":
     # 由于api限制, 该方法获取的followers不超过200个
     uid = 6126303533
-    net = Network.construct(uid, max_depth=2)
+    net = Network.construct(uid, max_depth=1)
     print(f'net contains {net.size} people, {net.edges} relations')
+    # pprint(net.relations)
     net.dump(f'{uid}.txt')
